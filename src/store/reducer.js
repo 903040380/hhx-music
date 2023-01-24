442621a950b78486bfd51eac23ed2b89d70881fe
HHX <903040380@qq.com>
2023-01-24 22:40:48 +0800

    feat: 完成歌手列表

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux-immutable'
 import { reducer as recommendReducer } from '../application/Recommend/store/index'
+import { reducer as singersReducer } from '../application/Singers/store/index'
 
 // redux也提供combineReducers，但是因为用了immutable所以需要特地引入redux-immutable
 export default combineReducers({
   // 合并不同模块 reducer
   recommend: recommendReducer,
+  singers: singersReducer,
 })
