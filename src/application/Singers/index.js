cd6061ed2766b7113dbff439c19d68ca3c88ca6a
HHX <903040380@qq.com>
2023-01-24 23:32:42 +0800

    feat: 完成上拉下载防抖加载与图片懒加载

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -14,6 +14,8 @@ import {
 } from './store/actionCreators'
 import Scroll from './../../baseUI/scroll/index'
 import { connect } from 'react-redux'
+import Loading from '../../baseUI/loading'
+import LazyLoad, { forceCheck } from 'react-lazyload'
 
 function Singers(props) {
   const { singerList, enterLoading, pullUpLoading, pullDownLoading, pageCount } = props
@@ -33,6 +35,14 @@ function Singers(props) {
     updateDispatch(val, alpha)
   }
 
+  const handlePullUp = () => {
+    pullUpRefreshDispatch(category, alpha, category === '', pageCount)
+  }
+
+  const handlePullDown = () => {
+    pullDownRefreshDispatch(category, alpha)
+  }
+
   useEffect(() => {
     getHotSingerDispatch()
     // eslint-disable-next-line
@@ -48,7 +58,9 @@ function Singers(props) {
           return (
             <ListItem key={item.accountId + '' + index}>
               <div className='img_wrapper'>
-                <img src={`${item.picUrl}?param=300x300`} width='100%' height='100%' alt='music' />
+                <LazyLoad placeholder={<img width='100%' height='100%' src={require('./singer.png')} alt='music' />}>
+                  <img src={`${item.picUrl}?param=300x300`} width='100%' height='100%' alt='music' />
+                </LazyLoad>{' '}
               </div>
               <span className='name'>{item.name}</span>
             </ListItem>
@@ -58,12 +70,22 @@ function Singers(props) {
     )
   }
 
+  // 因为列表不断新增所以在滚动时检查新增项的懒加载
   return (
     <NavContainer>
       <Horizen list={categoryTypes} title={'分类 (默认热门):'} handleClick={handleUpdateCatetory} oldVal={category}></Horizen>
       <Horizen list={alphaTypes} title={'首字母:'} handleClick={(val) => handleUpdateAlpha(val)} oldVal={alpha}></Horizen>
       <ListContainer>
-        <Scroll>{renderSingerList()}</Scroll>
+        <Scroll
+          onScroll={forceCheck}
+          pullUp={handlePullUp}
+          pullDown={handlePullDown}
+          pullUpLoading={pullUpLoading}
+          pullDownLoading={pullDownLoading}
+        >
+          {renderSingerList()}
+        </Scroll>
+        {enterLoading ? <Loading></Loading> : null}
       </ListContainer>
     </NavContainer>
   )
--- src/baseUI/scroll/index.js
+++ src/baseUI/scroll/index.js
@@ -1,7 +1,10 @@
-import React, { forwardRef, useState, useEffect, useRef, useImperativeHandle } from 'react'
-import PropTypes from 'prop-types'
+import React, { forwardRef, useState, useEffect, useRef, useImperativeHandle, useMemo } from 'react'
+import PropTypes, { bool } from 'prop-types'
 import BScroll from 'better-scroll'
 import styled from 'styled-components'
+import Loading from '../loading/index'
+import LoadingV2 from '../loading-v2/index'
+import { debounce } from '../../api/utils'
 
 const ScrollContainer = styled.div`
   width: 100%;
@@ -9,12 +12,42 @@ const ScrollContainer = styled.div`
   overflow: hidden;
 `
 
+const PullUpLoading = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 5px;
+  width: 60px;
+  height: 60px;
+  margin: auto;
+  z-index: 100;
+`
+
+export const PullDownLoading = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 10px;
+  height: 30px;
+  margin: auto;
+  z-index: 100;
+`
 const Scroll = forwardRef((props, ref) => {
   const [bScroll, setBScroll] = useState()
   const scrollContaninerRef = useRef()
 
   const { direction, click, refresh, pullUpLoading, pullDownLoading, bounceTop, bounceBottom } = props
   const { pullUp, pullDown, onScroll } = props
+
+  let pullUpDebounce = useMemo(() => {
+    return debounce(pullUp, 300)
+  }, [pullUp])
+  // 千万注意，这里不能省略依赖，
+  // 不然拿到的始终是第一次 pullUp 函数的引用，相应的闭包作用域变量都是第一次的，产生闭包陷阱。下同。
+
+  let pullDownDebounce = useMemo(() => {
+    return debounce(pullDown, 300)
+  }, [pullDown])
 
   // TODO 搞清楚ref什么时候赋值
   // 创建 better-scroll，渲染后才调用所以scrollContaninerRef肯定有值
@@ -57,30 +90,33 @@ const Scroll = forwardRef((props, ref) => {
   // 进行上拉到底的判断，调用上拉刷新的函数
   useEffect(() => {
     if (!bScroll || !pullUp) return
-    bScroll.on('scrollEnd', () => {
-      // 判断是否滑动到了底部
+    const handlePullUp = () => {
+      //判断是否滑动到了底部
       if (bScroll.y <= bScroll.maxScrollY + 100) {
-        pullUp()
+        pullUpDebounce()
       }
-    })
-    return () => {
-      bScroll.off('scrollEnd')
     }
-  }, [pullUp, bScroll])
+    bScroll.on('scrollEnd', handlePullUp)
+    // 解绑
+    return () => {
+      bScroll.off('scrollEnd', handlePullUp)
+    }
+  }, [pullUp, pullUpDebounce, bScroll])
 
   // 进行下拉的判断，调用下拉刷新的函数
   useEffect(() => {
     if (!bScroll || !pullDown) return
-    bScroll.on('touchEnd', (pos) => {
-      // 判断用户的下拉动作
-      if (pos.y > 50) {
-        pullDown()
+    const handlePullDown = (pos) => {
+      //判断用户的下拉动作
+      if (pos.y > 15) {
+        pullDownDebounce()
       }
-    })
-    return () => {
-      bScroll.off('touchEnd')
     }
-  }, [pullDown, bScroll])
+    bScroll.on('touchEnd', handlePullDown)
+    return () => {
+      bScroll.off('touchEnd', handlePullDown)
+    }
+  }, [pullDown, pullDownDebounce, bScroll])
 
   // 一般和 forwardRef 一起使用，ref 已经在 forWardRef 中默认传入
   useImperativeHandle(ref, () => ({
@@ -98,13 +134,27 @@ const Scroll = forwardRef((props, ref) => {
       }
     },
   }))
+  const PullUpdisplayStyle = pullUpLoading ? { display: '' } : { display: 'none' }
+  const PullDowndisplayStyle = pullDownLoading ? { display: '' } : { display: 'none' }
 
-  return <ScrollContainer ref={scrollContaninerRef}>{props.children}</ScrollContainer>
+  return (
+    <ScrollContainer ref={scrollContaninerRef}>
+      {props.children}
+      {/* 滑到底部加载动画 */}
+      <PullUpLoading style={PullUpdisplayStyle}>
+        <Loading></Loading>
+      </PullUpLoading>
+      {/* 顶部下拉刷新动画 */}
+      <PullDownLoading style={PullDowndisplayStyle}>
+        <LoadingV2></LoadingV2>
+      </PullDownLoading>
+    </ScrollContainer>
+  )
 })
 
 Scroll.propTypes = {
   direction: PropTypes.oneOf(['vertical', 'horizental']), // 滚动的方向
-  click: true, // 是否支持点击
+  click: bool, // 是否支持点击
   refresh: PropTypes.bool, // 是否刷新
   onScroll: PropTypes.func, // 滑动触发的回调函数
   pullUp: PropTypes.func, // 上拉加载逻辑
